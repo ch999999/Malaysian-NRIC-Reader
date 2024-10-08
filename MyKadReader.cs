23d2a0e4c305abe2e335e7cc9c860a04bb50074d
ch999999 <izakchordiscord@gmail.com>
2024-10-08 13:52:23 +0800

    made read image async

--- MyKadReader.cs
+++ MyKadReader.cs
@@ -2,6 +2,8 @@
 using PCSC;
 using PCSC.Iso7816;
 
+//This file documents the various APDU commands used to request data from MyKad
+
 namespace Malaysian_NRIC_Reader
 {
     internal class MyKadReader
@@ -9,7 +11,6 @@ namespace Malaysian_NRIC_Reader
         ISCardContext ctx = ContextFactory.Instance.Establish(SCardScope.System);
         string nameOfReader;
         IsoReader reader;
-        int yob = 0;
         byte[] sendData; //data command to send
         byte[] gotData; //data returned
         public MyKadReader()
@@ -27,7 +28,7 @@ namespace Malaysian_NRIC_Reader
                         releaseContextOnDispose: true);
         }
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
-        public void slctapp() //Select application
+        public void slctApp() //Select application
         {
             var apduSlctApp = new CommandApdu(IsoCase.Case4Extended, SCardProtocol.Any)
             {
@@ -51,7 +52,7 @@ namespace Malaysian_NRIC_Reader
             var responseAppResp = reader.Transmit(apduAppResp);
         }
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
-        public byte[] getdata(byte ssSetL1, byte ssSetL2, byte[] dataSlctF, byte ssGetD) //Transmit command data, and receive resulting data
+        public byte[] getData(byte ssSetL1, byte ssSetL2, byte[] dataSlctF, byte ssGetD) //Transmit command data, and receive resulting data
         {
 
             var apduSetL = new CommandApdu(IsoCase.Case4Extended, SCardProtocol.Any)
@@ -95,28 +96,28 @@ namespace Malaysian_NRIC_Reader
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
         public void initiateConnection() //Initiates connection to card, and selects application
         {
-            slctapp();
+            slctApp();
         }
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
         public string readName() //Reads and returns MyKad Name
         {
 
-            sendData = new byte[] { 0x01, 0x00, 0x01, 0x00, 0x03, 0x00, 0x28, 0x00 };
-            gotData = getdata(0x28, 0x00, sendData, 0x28);
+            sendData = [0x01, 0x00, 0x01, 0x00, 0x03, 0x00, 0x28, 0x00];
+            gotData = getData(0x28, 0x00, sendData, 0x28);
             return (Encoding.ASCII.GetString(gotData)).Trim();
         }
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
-        public string readICPassport() //Reads and returns IC number of MyKad
+        public string readNric() //Reads and returns IC number of MyKad
         {
-            sendData = new byte[] { 0x01, 0x00, 0x01, 0x00, 0x11, 0x01, 0x0D, 0x00 };
-            gotData = getdata(0x0D, 0x00, sendData, 0x0D);
+            sendData = [0x01, 0x00, 0x01, 0x00, 0x11, 0x01, 0x0D, 0x00];
+            gotData = getData(0x0D, 0x00, sendData, 0x0D);
             return (Encoding.ASCII.GetString(gotData)).Trim();
         }
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
         public string readGender() //Reads and returns gender 
         {
-            sendData = new byte[] { 0x01, 0x00, 0x01, 0x00, 0x1E, 0x01, 0x01, 0x00 };
-            gotData = getdata(0x01, 0x00, sendData, 0x01);
+            sendData = [0x01, 0x00, 0x01, 0x00, 0x1E, 0x01, 0x01, 0x00];
+            gotData = getData(0x01, 0x00, sendData, 0x01);
             string gender = Encoding.ASCII.GetString(gotData);
             if (gender == "L")
             {
@@ -128,41 +129,33 @@ namespace Malaysian_NRIC_Reader
             }
         }
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
-        public string readDOBsetYOB() //Reads the data of birth, and records the year of birth for later use
+        public string readDob() //Reads the data of birth
         {
-            sendData = new byte[] { 0x01, 0x00, 0x01, 0x00, 0x27, 0x01, 0x04, 0x00 };
-            gotData = getdata(0x04, 0x00, sendData, 0x04);
+            sendData = [0x01, 0x00, 0x01, 0x00, 0x27, 0x01, 0x04, 0x00];
+            gotData = getData(0x04, 0x00, sendData, 0x04);
             string dob = BitConverter.ToString(gotData);
             string[] DobArr = dob.Split("-");
             dob = DobArr[3] + "/" + DobArr[2] + "/" + DobArr[0] + DobArr[1];
-            yob = DateTime.Parse(dob).Year;
             return dob;
         }
-        /*------------------------------------------------------------------------------------------------------------------------------------------*/
-        public string calcAge() //calculates age of MyKad holder based on deduced year of birth. Does not consider month holder was born. Year only
-        {
-            DateTime now = DateTime.Now;
-            int age = now.Year - yob;
-            return age.ToString();
-        }
+        
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
         public string readRace() //Reads the ethnicity in the MyKad
         {
-            sendData = new byte[] { 0x01, 0x00, 0x01, 0x00, 0x5A, 0x01, 0x19, 0x00 };
-            gotData = getdata(0x19, 0x00, sendData, 0x19);
+            sendData = [0x01, 0x00, 0x01, 0x00, 0x5A, 0x01, 0x19, 0x00];
+            gotData = getData(0x19, 0x00, sendData, 0x19);
             return (Encoding.ASCII.GetString(gotData)).Trim();
         }
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
         public string readStreet() //Reads the street part of the address stored in MyKad
         {
-            sendData = new byte[] { 0x04, 0x00, 0x01, 0x00, 0x03, 0x00, 0x1E, 0x00 };
-            gotData = getdata(0x1E, 0x00, sendData, 0x1E);
+            sendData = [0x04, 0x00, 0x01, 0x00, 0x03, 0x00, 0x1E, 0x00];
+            gotData = getData(0x1E, 0x00, sendData, 0x1E);
             string Street1 = Encoding.ASCII.GetString(gotData);
             Street1 = Street1.Trim();
-            string[] strarr = Street1.Split();
 
-            sendData = new byte[] { 0x04, 0x00, 0x01, 0x00, 0x21, 0x00, 0x1E, 0x00 };
-            gotData = getdata(0x1E, 0x00, sendData, 0x1E);
+            sendData = [0x04, 0x00, 0x01, 0x00, 0x21, 0x00, 0x1E, 0x00];
+            gotData = getData(0x1E, 0x00, sendData, 0x1E);
             string Street2 = Encoding.ASCII.GetString(gotData);
             Street2 = Street2.Trim();
 
@@ -171,8 +164,8 @@ namespace Malaysian_NRIC_Reader
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
         public string readPostcode() //Reads the Postcode stored in MyKad
         {
-            sendData = new byte[] { 0x04, 0x00, 0x01, 0x00, 0x5D, 0x00, 0x03, 0x00 };
-            gotData = getdata(0x03, 0x00, sendData, 0x03);
+            sendData = [0x04, 0x00, 0x01, 0x00, 0x5D, 0x00, 0x03, 0x00];
+            gotData = getData(0x03, 0x00, sendData, 0x03);
             string postcode = (BitConverter.ToString(gotData)).Trim();
             postcode = postcode.Replace("-", "");
             postcode = postcode.Remove(postcode.Length - 1, 1);
@@ -181,34 +174,38 @@ namespace Malaysian_NRIC_Reader
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
         public string readCity() //Reads the City stored in MyKad
         {
-            sendData = new byte[] { 0x04, 0x00, 0x01, 0x00, 0x60, 0x00, 0x19, 0x00 };
-            gotData = getdata(0x19, 0x00, sendData, 0x19);
+            sendData = [0x04, 0x00, 0x01, 0x00, 0x60, 0x00, 0x19, 0x00];
+            gotData = getData(0x19, 0x00, sendData, 0x19);
             return (Encoding.ASCII.GetString(gotData)).Trim();
         }
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
         public string readState() //Reads the State stored in MyKad
         {
-            sendData = new byte[] { 0x04, 0x00, 0x01, 0x00, 0x79, 0x00, 0x1E, 0x00 };
-            gotData = getdata(0x1E, 0x00, sendData, 0x1E);
+            sendData = [0x04, 0x00, 0x01, 0x00, 0x79, 0x00, 0x1E, 0x00];
+            gotData = getData(0x1E, 0x00, sendData, 0x1E);
             return (Encoding.ASCII.GetString(gotData)).Trim();
         }
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
-        public Image readImage() //returns photo image of MyKad holder
+        public async Task<Image> readImage() //returns photo image of MyKad holder
         {
-            byte[] firstOff = { 0x03, 0xFD, 0xF7, 0xF1, 0xEB, 0xE5, 0xDF, 0xD9, 0xD3, 0xCD, 0xC7, 0xC1, 0xBB, 0xB5, 0xAF, 0xA9 };
-            byte[] secOff = { 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E };
-            byte[] arr = Array.Empty<byte>();
-            byte[] ph1Data = Array.Empty<byte>();
-            byte[] Ph1data = Array.Empty<byte>();
-            for (int i = 0; i <= 15; i++)
+            Image? img = null;
+            await Task.Run(() =>
             {
-                ph1Data = new byte[] { 0x02, 0x00, 0x01, 0x00, firstOff[i], secOff[i], 0xFA, 0x00 };
-                Ph1data = getdata(0xFA, 0x00, ph1Data, 0xFA);
-                arr = arr.Concat(Ph1data).ToArray();
-            }
-            Image img = (Bitmap)((new ImageConverter()).ConvertFrom(arr));
+                byte[] firstOffset = { 0x03, 0xFD, 0xF7, 0xF1, 0xEB, 0xE5, 0xDF, 0xD9, 0xD3, 0xCD, 0xC7, 0xC1, 0xBB, 0xB5, 0xAF, 0xA9 };
+                byte[] secondOffset = { 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E };
+                byte[] arr = [];
+                byte[] offsetData = Array.Empty<byte>();
+                byte[] photoData = Array.Empty<byte>();
+                for (int i = 0; i <= 15; i++)
+                {
+                    offsetData = [0x02, 0x00, 0x01, 0x00, firstOffset[i], secondOffset[i], 0xFA, 0x00];
+                    photoData = getData(0xFA, 0x00, offsetData, 0xFA);
+                    arr = arr.Concat(photoData).ToArray();
+                }
+                img = (Bitmap)(new ImageConverter().ConvertFrom(arr));
+            });
+            
             return img;
-
         }
     }
 }
--- MyKad_Reader_Form.cs
+++ MyKad_Reader_Form.cs
@@ -9,35 +9,38 @@ namespace Malaysian_NRIC_Reader
 
         private void MyKad_Reader_Form_Load(object sender, EventArgs e)
         {
-            cmbGender.SelectedIndex = 0;
+            cmbGender.SelectedIndex = 0; 
         }
 
-        private void btnRead_Click(object sender, EventArgs e)
+        private async void btnRead_Click(object sender, EventArgs e)
         {
             MyKadReader mkr = new MyKadReader();
             mkr.initiateConnection();
 
             txtName.Text = mkr.readName();
-            txtNric.Text = mkr.readICPassport();
+            txtNric.Text = mkr.readNric();
     
             string gender = mkr.readGender();
-            if (gender == "M")
+            if (gender == "M".ToUpper())
             {
                 cmbGender.SelectedIndex = 1;
-            }else if (gender == "F")
+            }else if (gender == "F".ToUpper())
             {
                 cmbGender.SelectedIndex = 2;
             }
 
-            dtpDob.Text = mkr.readDOBsetYOB();
-            lblAge.Text = $"Age: {mkr.calcAge()}";
+            string dob = mkr.readDob();
+            int age = DateTime.Now.Year - DateTime.Parse(dob).Year;
+
+            dtpDob.Text = dob;
+            lblAge.Text = $"Age: {age}";
             txtRace.Text = mkr.readRace();
             txtStreet.Text = mkr.readStreet();
             txtPostcode.Text = mkr.readPostcode();
             txtCity.Text = mkr.readCity();
             txtState.Text = mkr.readState();
 
-            imgHolder.Image = mkr.readImage();
+            imgHolder.Image = await mkr.readImage();
 
             mkr.release();
         }
